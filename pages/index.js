b24b6b526a90ccb42454bfafbf3631e3c3486f41
developercodingarrow <148421958+developercodingarrow@users.noreply.github.com>
2024-02-08 12:30:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Saransh Realtors</h1>
+        <h1>Saransh Realtors change is file and update</h1>
       </main>
     </>
   );
